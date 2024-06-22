d32300e04b8678779f0e82f5174bcf1abb18fd06
Avanthikach <151443416+Avanthikach@users.noreply.github.com>
2024-06-22 11:18:23 +0530

    Update header.tsx

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
                 <Row>
                     <Col lg={1} md={1} sm={12} xs={12}></Col>
                     <Col lg={10} md={10} sm={12} xs={12} className='p-3'>
-                        <h1>Hi, I&apos;m <span className={styles.nameText}>Avanthika Chennamadhavuni</span></h1>
+                        <h1>Hi, I&apos;m <span className={styles.nameText}>Avanthika</span></h1>
                         <h3>I&apos;m a <span className={`${styles.stackText} ${styles.textDecoration}`}>{text}</span></h3>
                         <h3>Welcome to my portfolio, where innovation meets craftsmanship. Explore my journey of creativity and expertise in building remarkable solutions.
                         </h3>
